9312a0c5a30d8ecc0f7ada28af07d348910cc6cd
prateek4506 <prateek4506@gmail.com>
2021-06-03 19:58:16 +0530

    uncommented avergae operation

--- src/App.java
+++ src/App.java
@@ -2,45 +2,44 @@ import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        
+
           // Take User input
-        Scanner sc = new Scanner(System.in);    
+        Scanner sc = new Scanner(System.in);
 
          ArrayTools obj = new ArrayTools();
 
         //CALCULATE AVERAGE
-        // System.out.println("Enter array size for AVERAGE operation: ");
-        // int arraySize = sc.nextInt();
-        // double arrAvg[] = new double[arraySize];
+        System.out.println("Enter array size for AVERAGE operation: ");
+        int arraySize = sc.nextInt();
+        double arrAvg[] = new double[arraySize];
 
-        // System.out.println("Enter values to calculate their average: ");
+        System.out.println("Enter values to calculate their average: ");
 
-        // for(int i=0;i<arraySize;i++)
-        // {
-        //     arrAvg[i] = (double)sc.nextDouble();
-        // }
-        // Double averageVal = obj.arrayAvg(arrAvg);
-        // System.out.println("Average of the input numbers is: " +averageVal);
+        for(int i=0;i<arraySize;i++)
+        {
+            arrAvg[i] = (double)sc.nextDouble();
+        }
+        Double averageVal = obj.arrayAvg(arrAvg);
+        System.out.println("Average of the input numbers is: " +averageVal);
 
-       
+
         //****AVERAGE LOGIC ENDS*******/
 
         //   //****ENCRYPTION STARTS********/
             System.out.println("Enter a shift value to encrypt: ");
-        
-            int shiftVal = sc.nextInt();      
-            
-            //int shiftVal = Integer.parseInt(shiftValString);  
-  
+
+            int shiftVal = sc.nextInt();
+
             System.out.println("Enter message to encrypt: ");
-            String inputVal = sc.nextLine();
+            String inputVal = sc.next();
+
             char[] encryptedVal =  obj.encrypt(shiftVal, inputVal);
             System.out.println(encryptedVal);
             String encString = String.valueOf(encryptedVal);
             char[] decryptedVal =  obj.decrypt(shiftVal, encString);
             System.out.println("Decrypting your secret message: ");
             System.out.println(decryptedVal);
-            
+
 
         //   //*****ENDS**********/
 
@@ -50,22 +49,22 @@ public class App {
 
         int arrSize = sc.nextInt();
 
-          System.out.println("Enter values to reverse an Array: ");
+          System.out.println("Enter characters one by one to reverse an Array: ");
 
           char[] revArr = new char[arrSize];
 
           for(int i=0;i<arrSize;i++)
           {
-            revArr[i] = sc.next().charAt(0);  
+            revArr[i] = sc.next().charAt(0);
           }
 
-         
+
           char[] reverseOrder =  obj.reverse(revArr);
-        
-        
-          System.out.println("Reverse of the input array: "+"\n"+String.valueOf(reverseOrder));       
-          
-          
+
+
+          System.out.println("Reverse of the input array: "+"\n"+String.valueOf(reverseOrder));
+
+
 
 
         //   //******REVERSE AN ARRAY-ENDS */
@@ -75,27 +74,27 @@ public class App {
 
          int contArrSize = sc.nextInt();
 
-         System.out.println("Enter characters or integers to test CONTAINS operation on an Array: ");
+         System.out.println("Enter characters or integers one by one to test CONTAINS operation on an Array: ");
 
          char[] inpArray = new char[contArrSize];
 
          for(int i=0;i<contArrSize;i++)
          {
-            inpArray[i] = sc.next().charAt(0);  
-         }       
+            inpArray[i] = sc.next().charAt(0);
+         }
 
-          System.out.println("Enter a value to find if it exists in the array you entered above: "); 
-        
-          char findChar = sc.next().charAt(0); 
-          
+          System.out.println("Enter a value to find if it exists in the array you entered above: ");
+
+          char findChar = sc.next().charAt(0);
+
           boolean exists = obj.arrayContains(inpArray, findChar);
           if(exists){
-          System.out.println("Kudos! it exists!"); } 
+          System.out.println("Kudos! it exists!"); }
           else{
-            System.out.println("No, value you entered does not exists :(");  
+            System.out.println("No, value you entered does not exists.");
           }
-   
+
     }
 
-    
+
 }
--- src/ArrayTools.java
+++ src/ArrayTools.java
@@ -1,100 +1,94 @@
-
-
 public class ArrayTools {
     char[] encrypt(int shiftVal, String inputVal)
-        {
-            inputVal = inputVal.toLowerCase();
-            char[] input = new char[inputVal.length()];
+    {
+        inputVal = inputVal.toLowerCase();
+        char[] input = new char[inputVal.length()];
 
-            
-            for (int i=0; i<inputVal.length(); i++) {
-                input[i] =  (char)(inputVal.charAt(i) + shiftVal);
 
-                //TO ONLY ENCRYPT AS ALPHABETS BUT NOT NEEDED
-                // if((int)(input[i]) > 122 )
-                // {
-                //    input[i] = (char)(input[i] - 26);
-                // }
-            }
-           
-           return input;
+        for (int i=0; i<inputVal.length(); i++) {
+            input[i] =  (char)(inputVal.charAt(i) + shiftVal);
+
+            //TO ONLY ENCRYPT AS ALPHABETS BUT NOT NEEDED
+            // if((int)(input[i]) > 122 )
+            // {
+            //    input[i] = (char)(input[i] - 26);
+            // }
         }
 
-        char[] decrypt(int shiftVal, String inputVal)
-        {
-            inputVal = inputVal.toLowerCase();
-            char[] input = new char[inputVal.length()];
+       return input;
+    }
 
-            
-            for (int i=0; i<inputVal.length(); i++) {
-                input[i] =  (char)(inputVal.charAt(i) - shiftVal);
+    char[] decrypt(int shiftVal, String inputVal)
+    {
+        inputVal = inputVal.toLowerCase();
+        char[] input = new char[inputVal.length()];
 
-                //TO ONLY ENCRYPT AS ALPHABETS BUT NOT NEEDED
-                // if((int)(input[i]) > 122 )
-                // {
-                //    input[i] = (char)(input[i] - 26);
-                // }
-            }
-           
-           return input;
+
+        for (int i=0; i<inputVal.length(); i++) {
+            input[i] =  (char)(inputVal.charAt(i) - shiftVal);
+
         }
 
-        Double sum=0.0;
-        Double arrayAvg(double[] arrVals)
+       return input;
+    }
+
+    Double sum=0.0;
+    Double arrayAvg(double[] arrVals)
+    {
+       for(int i=0;i< arrVals.length;i++){
+
+         sum = sum + arrVals[i];
+
+       }
+
+       double avg = sum / arrVals.length;
+       return avg;
+    }
+    static int count =0;
+    boolean arrayContains(char[] arr, char j)
+    {
+
+        for(int i=0;i<arr.length;i++)
         {
-           for(int i=0;i< arrVals.length;i++){
-            
-             sum = sum + arrVals[i];
+           if(arr[i] == j)
+           {
+               return true;
 
            }
 
-           double avg = sum / arrVals.length;
-           return avg;
+
         }
-        static int count =0;
-        boolean arrayContains(char[] arr, char j)
+
+    return false;
+
+    }
+
+    char[] reverse(char[] arr)
+    {
+        int len = arr.length;
+        char[] placeHolder = new char[len];
+        if(len%2==0)
         {
-            
-            for(int i=0;i<arr.length;i++)
+           for(int i=0;i<len/2;i++)
             {
-               if(arr[i] == j)
-               {
-                   return true;
-                   
-               }
-               
+                placeHolder[i] = arr[i];
+                arr[i] = arr[len-(i+1)];
+                arr[len-(i+1)] = placeHolder[i];
 
             }
+        }
+        else{
+            for(int i=0;i<(len/2) + 1;i++)
+            {
 
-        return false;
-            
+                placeHolder[i] = arr[i];
+                arr[i] = arr[len-(i+1)];
+                arr[len-(i+1)] = placeHolder[i];
+
+            }
         }
 
-        char[] reverse(char[] arr)
-        {
-            int len = arr.length;
-            char[] placeHolder = new char[len];
-            if(len%2==0)
-            {
-               for(int i=0;i<len/2;i++)
-                {
-                    placeHolder[i] = arr[i];
-                    arr[i] = arr[len-(i+1)];
-                    arr[len-(i+1)] = placeHolder[i];
-                
-                }
-            }
-            else{
-                for(int i=0;i<(len/2) + 1;i++)
-                {
-                  
-                    placeHolder[i] = arr[i];
-                    arr[i] = arr[len-(i+1)];
-                    arr[len-(i+1)] = placeHolder[i];
-                
-                }
-            }
+        return arr;
+    }
 
-            return arr;
-        }
 }
